68d791b23619f582fe0ec177f583da408b99c140
Seth Lachman <lachmanseth17@gmail.com>
2019-08-23 11:33:01 -0400

    Created character card component.

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <Card>
+      <Image src={`${props.character.image}`} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{props.character.name}</Card.Header>
+        <Card.Meta>
+          <span>{props.character.status}</span>
+        </Card.Meta>
+        <Card.Description>
+          Location: {props.character.location.name}
+        </Card.Description>
+        <Card.Description>
+          Origin: {props.character.origin.name}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Icon name="user" />
+        Episodes
+      </Card.Content>
+    </Card>
+  );
 }
