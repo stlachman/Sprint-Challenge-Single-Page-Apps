3d06f042cfcb0938bb4a3c5f00ede3f803a3d984
Seth Lachman <lachmanseth17@gmail.com>
2019-08-23 10:12:16 -0400

    Added basic routing and components for separate pages.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList.js";
+import LocationList from "./components/LocationList.js";
+import EpisodeList from "./components/EpisodeList.js";
 
 export default function App() {
   return (
     <main>
       <Header />
       <TabNav />
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/character" component={CharacterList} />
+      <Route path="/location" component={LocationList} />
+      <Route path="/episode" component={EpisodeList} />
     </main>
   );
 }
---
+++ src/components/LocationList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LocationList = () => {
+  return (
+    <div>
+      <h2>Location List</h2>
+    </div>
+  );
+};
+
+export default LocationList;
